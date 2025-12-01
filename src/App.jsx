f17593118c6b2df71ba96fa49bcb2c3fb2b422dc
luizbortolanza0-sys <luizbortolanza0@gmail.com>
2025-12-01 15:09:18 -0300

    feat: inicio de paginação

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home.jsx'
+
 function App() {
-  
+  return(
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
+  );
+
 }
 
 export default App
