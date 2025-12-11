78c77d08a4f181f6906e1dfbab70bc48d83f1054
luizbortolanza0-sys <luizbortolanza0@gmail.com>
2025-12-11 17:54:34 -0300

    feat: formatar o preço no componente ItemMap para exibição correta em moeda brasileira

--- src/Components/ItemMap.jsx
+++ src/Components/ItemMap.jsx
@@ -7,7 +7,10 @@ export function ItemMap({ lista }) {
     return (
       <InfoCard
         descricao={item.nome}
-        preco={item.valor}
+        preco={parseFloat(item.valor).toLocaleString("pt-BR", {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
         categoria={item.categoria}
         data={item.data}
         tipo={aux}
